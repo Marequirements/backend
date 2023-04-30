5bbd029029cd92444166a443353ba9032563b4b6
Marek Leščinský <marek.lescinsky@kosickaakademia.sk>
2023-04-30 22:49:39 +0200

    update user model

--- models/user.go
+++ models/user.go
@@ -5,10 +5,11 @@ import (
 )
 
 type User struct {
-	Id       bson.ObjectId `json:"id" bson:"_id"`
-	Username string        `json:"username" bson:"username"`
-	Password string        `json:"password" bson:"password"`
-	Role     string        `json:"role" bson:"role"`
-	Name     string        `json:"name" bson:"name"`
-	Surname  string        `json:"surname" bson:"surname"`
+	Id       bson.ObjectId   `json:"id" bson:"_id"`
+	Username string          `json:"username" bson:"username"`
+	Password string          `json:"password" bson:"password"`
+	Role     string          `json:"role" bson:"role"`
+	Name     string          `json:"name" bson:"name"`
+	Surname  string          `json:"surname" bson:"surname"`
+	Class    []bson.ObjectId `json:"class" bson:"class"`
 }
