56687f9b7397c4e61068496c74bcc159fe8b4389
Marek Lescinsky <marek.lescinsky@kosickaakademia.sk>
2023-05-08 22:26:08 +0200

    added teacher struct

--- model/user.go
+++ model/user.go
@@ -13,3 +13,12 @@ type User struct {
 	Surname  string             `json:"surname" bson:"surname"`
 	Class    primitive.ObjectID `json:"class" bson:"class"`
 }
+
+type Teacher struct {
+	Id       primitive.ObjectID `json:"id" bson:"_id"`
+	Username string             `json:"username" bson:"username"`
+	Password string             `json:"password" bson:"password"`
+	Role     string             `json:"role" bson:"role"`
+	Name     string             `json:"name" bson:"name"`
+	Surname  string             `json:"surname" bson:"surname"`
+}
