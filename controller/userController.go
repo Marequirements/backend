cb02ad96df8a3cf162c987c720bf0d309b05ab8d
Adam <adam.milko@kosickaakademia.sk>
2023-05-19 18:37:17 +0200

    Student api fix according to documentation

--- controller/userController.go
+++ controller/userController.go
@@ -197,124 +197,74 @@ func (sc *StudentController) HandleLogout(w http.ResponseWriter, r *http.Request
 func (sc *StudentController) HandleAddStudent(w http.ResponseWriter, r *http.Request) {
 	log.Println("Function HandleAddStudent called")
 
-	log.Println("HandleAddStudent: Getting authHeader")
-	// Get the userToken from the header
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
-		log.Println("HandleAddStudent: AuthHeader is empty, header= ", authHeader)
-		http.Error(w, "Authorization header not provided", http.StatusBadRequest)
+		respondWithError(w, http.StatusBadRequest, "Authorization header not provided")
 		return
 	}
-	log.Println("HandleAddStudent: Got authHeader, header= ", authHeader)
-
-	log.Println("HandleAddStudent: Getting user token")
 	userToken := strings.TrimPrefix(authHeader, "Bearer ")
-	log.Println("HandleAddStudent: user token= ", userToken)
 
-	log.Println("HandleAddStudent: Getting username form token= ", userToken)
-	// Get the username associated with the userToken
 	username, err := sc.ts.GetUsernameByToken(userToken)
 	if err != nil {
-		log.Println("HandleAddStudent: Could not get username from token= ", userToken)
-		http.Error(w, "Invalid userToken", http.StatusUnauthorized)
+		respondWithError(w, http.StatusUnauthorized, "Token is invalid")
 		return
 	}
-	log.Println("HandleAddStudent: Got username= ", username, " form token= ", userToken)
 
-	log.Println("HandleAddStudent: Checking role of user= ", username)
-	// Check if the user has the teacher role
 	role, err := sc.GetUserRole(username)
-	if err != nil {
-		log.Println("HandleAddStudent: Could not get role from user= ", username)
-		http.Error(w, "Failed getting username", http.StatusInternalServerError)
+	if err != nil || role != "teacher" {
+		respondWithError(w, http.StatusForbidden, "User does not have permission for this request")
 		return
 	}
-	if role != "teacher" {
-		log.Println("HandleAddStudent: User= ", username, " does not have teacher role, role= ", role)
-		http.Error(w, "Unauthorized access", http.StatusForbidden)
-		return
-	}
-	log.Println("HandleAddStudent: User= ", username, " has role= ", role)
 
-	log.Println("HandleAddStudent: Getting data from body")
-	// Decode the student details from the request body
 	var student model.NewStudent
 	if err := json.NewDecoder(r.Body).Decode(&student); err != nil {
-		log.Println("HandleAddStudent: Failed to get data from body= ", r.Body)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		respondWithError(w, http.StatusBadRequest, "JSON parameters not provided")
 		return
 	}
-	log.Println("HandleAddStudent: Successfully got data from body= ", r.Body)
 
-	log.Println("HandleAddStudent: Getting objectid of class= ", student.ClassTitle)
 	classID, err := sc.GetClassIDByTitle(student.ClassTitle)
 	if err != nil {
-		log.Println("HandleAddStudent: Could not get objectid of class= ", student.ClassTitle)
-		http.Error(w, "Invalid class title", http.StatusBadRequest)
+		respondWithError(w, http.StatusNotFound, "Class does not exist")
 		return
 	}
-	log.Println("HandleAddStudent: Objectid found, objectid =", classID, " calss= ", student.ClassTitle)
 
 	student.Class = classID
-
-	log.Println("HandleAddStudent: Adding student= ", student.Username, " to database")
-	// Add the student to the database
 	err = sc.AddStudent(student)
 	if err != nil {
-		log.Println("HandleAddStudent: Failed to add student ", student.Username, " to database")
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		if err.Error() == "Username already taken" {
+			respondWithError(w, http.StatusConflict, "Username already exists")
+		} else {
+			respondWithError(w, http.StatusInternalServerError, "Unexpected server error")
+		}
 		return
 	}
 
-	log.Println("HandleAddStudent: Student ", student, " successfully added")
-
-	util.WriteSuccessResponse(w, http.StatusCreated, nil)
+	w.WriteHeader(http.StatusCreated)
 }
+
 func (sc *StudentController) AddStudent(student model.NewStudent) error {
 	log.Println("Function AddStudent called")
-	// Get a handle to the "user" collection
 	collection := sc.db.Database("BrainBoard").Collection("user")
 
-	log.Println("AddStudent: Checking if username= ", student.Username, " is already in database")
-	// Check if the username is already taken
 	filter := bson.M{"username": student.Username}
 	var existingUser model.NewStudent
 	err := collection.FindOne(context.Background(), filter).Decode(&existingUser)
 	if err != mongo.ErrNoDocuments {
-		log.Println("AddStudent: Username= ", student.Username, " is already in database")
 		return fmt.Errorf("Username already taken")
 	}
-	log.Println("AddStudent: Username=", student.Username, " not yet in database")
-	/*var existingStudent model.Student
-	err = collection.FindOne(context.Background(), filter).Decode(&existingStudent)
-	if err != mongo.ErrNoDocuments {
-		return err
-	}
-	// Insert the new subject into the collection
-	_, err = collection.InsertOne(context.Background(), existingStudent)
-	return err*/
 
-	log.Println("AddStudent: Hashing password")
-	// Hash the default password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("brainboard"), bcrypt.DefaultCost)
 	if err != nil {
-		log.Println("AddStudent: Failed to hash password ")
 		return err
 	}
-	log.Println("AddStudent: Password hashed=", hashedPassword)
 
-	// Set the hashed password and role
 	student.Password = string(hashedPassword)
 	student.Role = "student"
 
-	log.Println("AddStudent: Inserting student to database")
-	// Insert the new student into the collection
 	_, err = collection.InsertOne(context.Background(), student)
-	if err == nil {
-		log.Println("AddStudent: Student ", student, " added to database")
-	}
 	return err
 }
+
 func (sc *StudentController) GetUserRole(username string) (string, error) {
 	log.Println("Function GetUserRole called")
 	// Get a handle to the "user" collection.
@@ -344,175 +294,136 @@ func (sc *StudentController) GetUserRole(username string) (string, error) {
 func (sc *StudentController) HandleDeleteStudent(w http.ResponseWriter, r *http.Request) {
 	log.Println("Function HandleDeleteStudent called")
 
-	log.Println("HandleDeleteStudent: Getting authHeader")
 	// Get the userToken from the header
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
-		log.Println("HandleDeleteStudent: authHeader is empty= ", authHeader)
-		http.Error(w, "Authorization header not provided", http.StatusBadRequest)
+		respondWithError(w, http.StatusBadRequest, "Authorization header not provided")
 		return
 	}
-	log.Println("HandleDeleteStudent: Got authHeader= ", authHeader)
 	userToken := strings.TrimPrefix(authHeader, "Bearer ")
 
-	log.Println("HandleDeleteStudent: User token is ", userToken)
-
-	log.Println("HandleDeleteStudent: Getting username from from user token= ", userToken)
 	// Get the username associated with the userToken
 	username, err := sc.ts.GetUsernameByToken(userToken)
 	if err != nil {
-		log.Println("HandleDeleteStudent: Failed to get username from token= ", userToken)
-		http.Error(w, "Invalid userToken", http.StatusUnauthorized)
+		respondWithError(w, http.StatusUnauthorized, "Token is invalid")
 		return
 	}
 
-	log.Println("HandleDeleteStudent: Got username= ", username, " for token ", userToken)
-
-	log.Println("HandleDeleteStudent: Checking the role of user= ", username)
 	// Check if the user has the teacher role
 	role, err := sc.GetUserRole(username)
 	if err != nil || role != "teacher" {
-		log.Println("HandleDeleteStudent: Failed to get role of user= ", username)
-		http.Error(w, "Unauthorized access", http.StatusForbidden)
+		respondWithError(w, http.StatusForbidden, "User does not have permission for this request")
 		return
 	}
-	log.Println("HandleDeleteStudent: role= ", role, " username= ", username)
 
-	log.Println("HandleDeleteStudent: Getting student username from body")
 	// Get the student's ID from the request body
 	var body struct {
 		Username string `json:"username"`
 	}
 
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		log.Println("HandleDeleteStudent: Failed to get username ")
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		respondWithError(w, http.StatusBadRequest, "JSON parameters not provided")
 		return
 	}
-	log.Println("HandleDeleteStudent: username from body is ", body.Username)
-	log.Println("HandleDeleteStudent: Deleting user= ", body.Username)
+
 	// Delete the student from the database
 	err = sc.DeleteStudent(body.Username)
 	if err != nil {
-		log.Println("HandleDeleteStudent: Failed to delete user= ", body.Username)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		if err.Error() == "no student found with the specified username" {
+			respondWithError(w, http.StatusNotFound, "The Subject does not exists")
+		} else {
+			respondWithError(w, http.StatusInternalServerError, "Unexpected server error")
+		}
 		return
 	}
-	log.Println("HandleDeleteStudent: user= ", body.Username, " was deleted")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusNoContent)
 }
+
 func (sc *StudentController) DeleteStudent(username string) error {
 	log.Println("Function DeleteStudent called")
+
 	// Get a handle to the "user" collection
 	collection := sc.db.Database("BrainBoard").Collection("user")
 
-	log.Println("DeleteStudent: Deleting student= ", username, " from database")
 	// Delete the student from the collection
 	filter := bson.M{"username": username, "role": "student"}
 	res, err := collection.DeleteOne(context.Background(), filter)
 	if err != nil {
-		log.Println("DeleteStudent: Failed to delete user= ", username)
 		return err
 	}
 
 	if res.DeletedCount == 0 {
-		log.Println("DeleteStudent: Student with username= ", username, " was not found in database")
 		return fmt.Errorf("no student found with the specified username")
 	}
 
-	log.Println("DeleteStudent: user= ", username, " was deleted from database")
 	return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	resp := map[string]string{"error": message}
+	jsonResp, _ := json.Marshal(resp)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(jsonResp)
 }
 
 func (sc *StudentController) HandleEditStudent(w http.ResponseWriter, r *http.Request) {
 	log.Println("Function HandleEditStudent called")
 
-	log.Println("HandleEditStudent: Getting authHeader ")
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" {
-		log.Println("HandleEditStudent: authHeader is empty= ", authHeader)
-		http.Error(w, "Authorization header not provided", http.StatusBadRequest)
+		respondWithError(w, http.StatusBadRequest, "Authorization header not provided")
 		return
 	}
-	log.Println("HandleEditStudent: Got authHeader= ", authHeader)
 	userToken := strings.TrimPrefix(authHeader, "Bearer ")
 
-	log.Println("HandleEditStudent: user token= ", userToken)
-
-	log.Println("HandleEditStudent: Getting username from token, user token= ", userToken)
 	username, err := sc.ts.GetUsernameByToken(userToken)
 	if err != nil {
-		log.Println("HandleEditStudent: Failed to get username from token= ", userToken)
-		http.Error(w, "Invalid userToken", http.StatusUnauthorized)
+		respondWithError(w, http.StatusUnauthorized, "Token is invalid")
 		return
 	}
 
-	log.Println("HandleEditStudent: username = ", username, "token= ", userToken)
-
-	log.Println("HandleEditStudent: Getting role of user= ", username)
 	role, err := sc.GetUserRole(username)
-	if err != nil {
-		log.Println("HandleEditStudent: Failed to get role if user= ", username)
-		http.Error(w, "Unauthorized access", http.StatusForbidden)
-		return
-	}
-	if role != "teacher" {
-		log.Println("HandleEditStudent: user= ", username, " does not have role of teacher, role= ", role)
-		http.Error(w, "Unauthorized access", http.StatusForbidden)
+	if err != nil || role != "teacher" {
+		respondWithError(w, http.StatusForbidden, "User does not have permission for this request")
 		return
 	}
 
-	log.Println("HandleEditStudent: user= ", username, " role= ", role)
-
-	log.Println("HandleEditStudent: Getting data from body")
 	var editRequest model.EditStudentRequest
 	if err := json.NewDecoder(r.Body).Decode(&editRequest); err != nil {
-		log.Println("HandleEditStudent: Failed to get information from body= ", r.Body)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		respondWithError(w, http.StatusBadRequest, "JSON parameters not provided")
 		return
 	}
-	log.Println("HandleEditStudent: Got data from body= ", r.Body)
 
-	log.Println("HandleEditStudent: Getting class objectid by title= ", editRequest.NewStudent.ClassTitle)
-	// Get the class ID by the class title
 	classID, err := sc.GetClassIDByTitle(editRequest.NewStudent.ClassTitle)
 	if err != nil {
-		log.Println("HandleEditStudent: Failed to get objectid of class= ", editRequest.NewStudent.ClassTitle)
-		http.Error(w, "Invalid class title", http.StatusBadRequest)
+		respondWithError(w, http.StatusNotFound, "Class does not exist")
 		return
 	}
 
-	log.Println("HandleEditStudent: ObjectId of class= ", editRequest.NewStudent.ClassTitle, " : ", classID)
-
-	// Set the class field of the NewStudent struct
 	editRequest.NewStudent.Class = classID
-	log.Println("HandleEditStudent: Editing student= ", editRequest)
 	err = sc.EditStudent(editRequest)
 	if err != nil {
-		log.Println("HandleEditStudent: Failed to edit student= ", editRequest)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		if err.Error() == "No student found with the specified username" {
+			respondWithError(w, http.StatusNotFound, "Username does not exist")
+		} else {
+			respondWithError(w, http.StatusInternalServerError, "Unexpected server error")
+		}
 		return
 	}
 
-	log.Println("HandleEditStudent: Successfully edited student= ", editRequest)
-
-	util.WriteSuccessResponse(w, http.StatusNoContent, nil)
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func (sc *StudentController) EditStudent(editRequest model.EditStudentRequest) error {
 	log.Println("Function EditStudent called")
 	collection := sc.db.Database("BrainBoard").Collection("user")
 
-	log.Println("EditStudent: Old username= ", editRequest.OldStudentUsername)
-	log.Println("EditStudent: New username= ", editRequest.NewStudent.Username)
-
-	log.Println("EditStudent: Hashing the new password")
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(editRequest.NewStudent.Password), bcrypt.DefaultCost)
 	if err != nil {
-		log.Println("EditStudent: Failed to hash password= ", editRequest.NewStudent.Password)
 		return err
 	}
-	log.Println("EditStudent: new password hashed")
 
 	update := bson.M{
 		"$set": bson.M{
@@ -523,20 +434,17 @@ func (sc *StudentController) EditStudent(editRequest model.EditStudentRequest) e
 			"class":    editRequest.NewStudent.Class,
 		},
 	}
-	log.Println("EditStudent: Updating user= ", editRequest.OldStudentUsername, "to new user= ", editRequest.NewStudent)
-	filter := bson.M{"username": editRequest.OldStudentUsername, "role": "student"} // Use OldStudentUsername here
+	filter := bson.M{"username": editRequest.OldStudentUsername, "role": "student"}
 
 	res, err := collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
-		log.Println("EditStudent: Failed to udate user= ", editRequest.OldStudentUsername)
 		return err
 	}
 
 	if res.MatchedCount == 0 {
-		log.Println("EditStudent: No student found with the specified username= ", editRequest.OldStudentUsername)
 		return fmt.Errorf("No student found with the specified username")
 	}
-	log.Println("EditStudent: Successfully udate user= ", editRequest.OldStudentUsername, " to new data= ", editRequest.NewStudent)
+
 	return nil
 }
 
