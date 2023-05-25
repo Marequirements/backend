69f7574bfdd570bb56500a700b9d1ae24928cf7c
Marek Lescinsky <marek.lescinsky@kosickaakademia.sk>
2023-05-26 00:44:18 +0200

    add new endoint /teacher/dashboard

--- main.go
+++ main.go
@@ -38,9 +38,10 @@ func main() {
 	ts := token.GetTokenStorageInstance()
 
 	//Created user controller
-	uc := controller.NewStudentController(client, token.GetTokenStorageInstance())
+	uc := controller.NewStudentController(client, ts)
 	subjectc := controller.NewSubjectController(client, ts, uc)
 	taskController := controller.NewTaskController(client, ts, uc)
+	dashBoardController := controller.NewDashBoardController(client, ts, uc, taskController)
 
 	router.Post("/login", uc.HandleLogin)
 
@@ -57,6 +58,8 @@ func main() {
 	router.Get("/tasks", taskController.GetTasks)
 	router.Get("/task", subjectc.HandleGetFormSubjects)
 	router.Get("/task/{classTitle}", taskController.HandleTeacherTasks)
+
+	router.Get("/teacher/dashboard", dashBoardController.HandleTeacherDashBoard)
 
 	log.Println("Starting server...")
 	err = http.ListenAndServe(":8080", router)
